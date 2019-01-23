a20628e8768e250390a5a9acabf102e04e7bafb4
Jordan Stoddard <jjsincorporated@gmail.com>
2019-01-23 14:35:22 -0800

    testing html-style jsx

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,8 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-    return <div>Hi there!</div>
-}
+    return (
+        <div>
+            <label class="label" for="name">Enter Name:</label>
+            <input type="text" id="name"/>
+            <button style="background-color: blue; color: white;">Submit</button>
+        </div>
+    );
+};
 
 ReactDOM.render(
 <App/>,
