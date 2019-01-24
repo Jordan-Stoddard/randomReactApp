29f4740a5f6775da0487f29158e0deedadb6898a
Jordan Stoddard <jjsincorporated@gmail.com>
2019-01-24 15:18:32 -0800

    slight change to index

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+
+
 
 const App = () => {
-    return (
-        <div>
-            <label class="label" for="name">Enter Name:</label>
-            <input type="text" id="name"/>
-            <button style="background-color: blue; color: white;">Submit</button>
-        </div>
-    );
+
+    const buttonText = {text: 'click me'}
+    const labelText = 'Enter Name'
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input type="text" id="name" />
+      <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText.text}</button>
+    </div>
+  );
 };
 
-ReactDOM.render(
-<App/>,
-document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector("#root"));
